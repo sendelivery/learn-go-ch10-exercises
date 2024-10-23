f1f438d2c66821c8c995eec60dcec4202b975603
Sebastian Chumaceiro <seb.chumaceiro@deliveroo.co.uk>
2024-10-23 12:57:20 +0100

    chore: added godocs to main package and `Add` function

--- main.go
+++ main.go
@@ -1,5 +1,12 @@
+// Package main contains helper arithmetic functions on integers that can be
+// used to improve legibility when performing basic mathematical operations.
 package main
 
+// Add takes in two integer values and returns the result of their addition.
+//
+// Click [here] to learn more about addition.
+//
+// [here]: https://www.mathisfun.com/numbers/addition.html
 func Add(a, b int) int {
 	return a + b
 }
