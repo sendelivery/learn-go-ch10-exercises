b38fab49445036917d9e2967725c2366c2b34bf0
Sebastian Chumaceiro <seb.chumaceiro@deliveroo.co.uk>
2024-10-23 13:08:07 +0100

    feat: make `Add` generic, introduce `Number` type

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/sendelivery/learn_go_ch10_exercises
 
 go 1.23.2
+
+require golang.org/x/exp v0.0.0-20241009180824-f66d83c29e7c
--- main.go
+++ main.go
@@ -1,12 +1,19 @@
-// Package main contains helper arithmetic functions on integers that can be
-// used to improve legibility when performing basic mathematical operations.
+// Package main contains helper arithmetic functions that can be used to
+// improve legibility when performing basic mathematical operations.
 package main
 
-// Add takes in two integer values and returns the result of their addition.
+import "golang.org/x/exp/constraints"
+
+// Number represents any integer or floating point number.
+type Number interface {
+	constraints.Integer | constraints.Float
+}
+
+// Add takes in two [Number] values and returns the result of their addition.
 //
 // Click [here] to learn more about addition.
 //
 // [here]: https://www.mathisfun.com/numbers/addition.html
-func Add(a, b int) int {
+func Add[T Number](a, b T) T {
 	return a + b
 }
